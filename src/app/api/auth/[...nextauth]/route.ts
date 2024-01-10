63a0ff125945104493c10376e6d6bf025d74f092
yk-jo <yk.jo@the-flan.com>
2024-01-10 17:54:48 +0900

    .

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -9,6 +9,11 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
